5f6f91e830f080b6faf002ed9f3f00a6b074a0a3
alperen-sakin <azapkaan@gmail.com>
2026-01-22 04:37:24 +0100

    Add mobile menu functionality and integrate framer-motion for animations

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,35 @@
 "use client";
 
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NAV_LINKS, SOCIAL_LINKS } from "@/constants/navbarItems";
+import { motion } from "motion/react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleClose = () => {
+  const toggleMenu = () => {
     setIsOpen((prev) => !prev);
   };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [isOpen]);
 
   return (
     <div>
       <nav className="w-full border-b border-cyan-glow">
-        <div className="max-w-[1440px] mx-auto px-6 h-20 flex items-center relative justify-center md:justify-between">
+        <div className="max-w-360 mx-auto px-6 h-20 flex items-center  justify-center md:justify-between">
           {/* Logo Section */}
-          <div className="flex items-center absolute mx-auto md:relative md:mx-0 gap-4">
+          <div className="flex items-center  mx-auto  md:mx-0 gap-4">
             <div
               className="w-10 h-10 bg-white"
               style={{
@@ -62,8 +75,8 @@ const Navbar = () => {
 
           {/* Mobile menu button */}
           <div
-            onClick={() => handleClose()}
-            className="md:hidden h-10 w-10 bg-title-grey absolute right-6 hover:bg-cyan-glow transition-all duration-300"
+            onClick={() => toggleMenu()}
+            className="md:hidden h-10 w-10 bg-title-grey   hover:bg-cyan-glow transition-all duration-300"
             style={{
               maskImage: `${isOpen ? "url(/close-icon.svg)" : "url(/menu-icon.svg)"}`,
               maskRepeat: "no-repeat",
@@ -72,6 +85,44 @@ const Navbar = () => {
           ></div>
         </div>
       </nav>
+
+      {/* Mobile Menu */}
+
+      <motion.div
+        initial={{ x: "100%" }}
+        animate={{ x: isOpen ? 0 : "100%" }}
+        transition={{ type: "tween" }}
+        className="md:hidden fixed top-20 left-0 w-full z-50"
+      >
+        <ul className="md:hidden flex flex-col items-center justify-center  gap-8 py-6 bg-black/95 backdrop-blur-md border-b border-cyan-glow">
+          {NAV_LINKS.map((link) => (
+            <li
+              key={link.key}
+              onClick={() => setIsOpen(false)}
+              className="text-title-grey text-2xl font-semibold hover:text-cyan-glow transition-all duration-300"
+            >
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+
+          {/* Social Links */}
+          <li className="flex w-full justify-center gap-6 border-t border-cyan-glow pt-6">
+            {SOCIAL_LINKS.map((social) => (
+              <Link
+                onClick={() => setIsOpen(false)}
+                className="w-10 h-10 bg-title-grey hover:bg-cyan-glow transition-all duration-300"
+                key={social.key}
+                href={social.href}
+                style={{
+                  maskImage: social.maskImage,
+                  maskRepeat: "no-repeat",
+                  maskSize: "contain",
+                }}
+              ></Link>
+            ))}
+          </li>
+        </ul>
+      </motion.div>
     </div>
   );
 };
