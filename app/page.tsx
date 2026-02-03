e5f914879c599548e0d7e840d494ddab4827e1a7
alperen-sakin <azapkaan@gmail.com>
2026-02-03 05:59:26 +0100

    feat: implement Hero component with spotlight effects and theme provider integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Hero from "@/components/Hero";
 import React from "react";
 
-const page = () => {
-  return <div></div>;
-};
-
-export default page;
+export default function Home() {
+  return (
+    <main className="relative bg-dark-bg flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
+      </div>
+    </main>
+  );
+}
